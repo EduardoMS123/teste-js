6cc2bf049fed92de4008cc5f929f5697235bec54
EduardoMS <eduardo.montanha.santos@escola.pr.gov.br>
2025-09-11 12:35:03 -0300

    Add files via upload

--- Tabela-Nutri-o-main/js/principal.js
+++ Tabela-Nutri-o-main/js/principal.js
@@ -1,49 +1,60 @@
   
     // alterar título e subtítulo, busca os elementos a partir da classe e atribui novo valor
-  var pacientes = document.querySelector(".titulo-secundario");
+  var tSecundario = document.querySelector(".titulo-secundario");
     var titulo = document.querySelector(".titulo");
     
-    pacientes.textContent = "Meus clienbinhos";
+    tSecundario.textContent = "Meus clienbinhos";
     titulo.textContent = "TEMM Nutrição";
 
-
     // PACIENTE PAULO, cria a variavel paciente, busca o elemento a partir da classe e altera seu conteudo   
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+    var pacientes = document.querySelectorAll(".paciente");
+   
+    for(var i = 0; i < pacientes.length; i++){
+    var paciente = pacientes[i]
 
-    // busca o elemento da altura e altera seu conteudo
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+            var tdPeso = paciente.querySelector(".info-peso");
+            var peso = tdPeso.textContent;
+    
+        // busca o elemento da altura e altera seu conteudo
+            var tdAltura = paciente.querySelector(".info-altura");
+            var altura = tdAltura.textContent;
+    
+        // busca o elemento do IMC e altera seu conteudo
+            var IMC = peso / (altura * altura);
+        
+        //valores booleanos
+            var pesoValido = true;
+            var alturaValida = true;
+        
+            if(pesoValido && alturaValida){
+                var tdIMC = paciente.querySelector(".info-imc")
+                tdIMC.textContent = IMC.toFixed(2);
+            }
 
-    // busca o elemento do IMC e altera seu conteudo
-var IMC = peso / (altura * altura);
-var tdIMC = paciente.querySelector(".info-imc")
-tdIMC.textContent = IMC;
+        //condição para definir se peso ou altura são inválidos
+            if(peso <= 0 || peso >= 1000 ){
+                console.log("Peso Inválido");
+                var pesoValido = false;
+                tdIMC.textContent = "Peso Inválido"
+                paciente.classList.add("paciente-invalido");
+            }
+        
+            if(altura <= 0 || altura >= 4.00){
+                console.log("Altura Inválida");
+                var alturaValida = false;
+                tdIMC.textContent = "Altura Inválida"
+                paciente.classList.add("paciente-invalido");
+            }
+                 
+}
 
-console.log(IMC);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener('click', 
+            function(event){
+                event.preventDefault();
+                console.log("ME CLIKO");
+            }   
+)
 
 
 
---
+++ Tabela-Nutri-o-main/js/teste.js
@@ -0,0 +1,22 @@
+function Evolucao(){
+    var Rowlet = 5
+    var Dartrix = Rowlet * 2
+    var Decidueye = Dartrix * 2
+    var evolucaoFinal = Decidueye + 5
+    console.log(evolucaoFinal);
+}
+
+Evolucao();
+
+if(Evolucao >= 25){
+console.log("Evoluiu ele");
+var evolucaoFinal = true;
+}else{
+    console.log("Não Evoluiu ele");
+    var evolucaoFinal = false;
+}
+
+
+
+
+
