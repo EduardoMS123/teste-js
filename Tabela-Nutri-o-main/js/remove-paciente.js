2f1fe2dbf3ccc0806ee26f17d73a018bdb793176
EduardoMS <eduardo.montanha.santos@escola.pr.gov.br>
2025-11-05 09:48:42 -0300

    Add files via upload

--- Tabela-Nutri-o-main/js/remove-paciente.js
+++ Tabela-Nutri-o-main/js/remove-paciente.js
@@ -1,16 +1,19 @@
-pacientes.forEach(function(paciente){
-    paciente.addEventListener('dblclick', function (){
-       this.remove();
-    });
+const tabela = document.querySelector("tbody");
+
+tabela.addEventListener('dblclick', function(event){
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
 });
 
 
-
-
-
-
-
-
+/*pacientes.forEach(function(paciente){
+    paciente.addEventListener('dblclick', function (){
+       this.remove();
+    });
+});*/
 
 
 
